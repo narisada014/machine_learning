30410036873e13485309a19eb2413223135cc8ae
narisada014 <nari.reishi@gmail.com>
2019-11-17 21:05:34 +0900

    fix original logic

--- card_game.py
+++ card_game.py
@@ -1,28 +1,30 @@
-# n = input()
-#
-# a = input().split()
-#
-# arr_length = len(a)
-# alice_point = 0
-# bob_point = 0
-#
-# for _ in range(arr_length):
-#     if len(a) is 0:
-#         break
-#     alice_max = max(a)
-#     alice_point += int(max(a))
-#     a.remove(alice_max)
-#
-#     if len(a) is 0:
-#         break
-#     bob_max = max(a)
-#     bob_point += int(max(a))
-#     a.remove(bob_max)
-#
-#
-# print(alice_point - bob_point)
+# A. 1
+n = input()
+
+a = list(map(int, input().split()))
+
+arr_length = len(a)
+alice_point = 0
+bob_point = 0
+
+for _ in range(arr_length):
+    if len(a) is 0:
+        break
+    alice_max = max(a)
+    alice_point += int(max(a))
+    a.remove(alice_max)
+
+    if len(a) is 0:
+        break
+    bob_max = max(a)
+    bob_point += int(max(a))
+    a.remove(bob_max)
+
+
+print(alice_point - bob_point)
 # メモリ使いすぎ
 
+# A. 2
 n = int(input())
 # 標準入力される数値をスプリットと同時にintegerにしてlistにする
 # そのlistをsortしてreverseしてやる。sortだと小さい順番になっているのでreverse
